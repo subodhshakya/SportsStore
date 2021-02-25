9cc960da7743c9f05686a9607b41dc0fca521f46
Subodh Shakya <sshakya@onesourcevirtual.com>
2021-02-25 10:27:46 -0600

    Added connection string, registered DB context in startup, added repository class for DB access implementation

---
+++ SportsStore/Models/IStoreRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public interface IStoreRepository
+    {
+        // Allows to obtain sequence of Product objects.
+        // IQueryable derives from IEnumerable<T> interface and represents a collection
+        // of objects that can be queried.
+        // Class that depends on IProductRepository interface can obtain Product objects
+        // without needing to know the details of how they are stored or implemented.
+        IQueryable<Product> Products { get; }
+    }
+}
