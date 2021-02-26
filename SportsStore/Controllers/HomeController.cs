78626b4453755484e9e6838295abca6fc4bb0cb8
Subodh Shakya <subodhshakya@live.com>
2021-02-26 00:30:00 -0600

    Added pagination

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -12,16 +12,23 @@ namespace SportsStore.Controllers
     public class HomeController : Controller
     {
         private IStoreRepository repository;
+        public int PageSize = 4;
 
         public HomeController(IStoreRepository repo)
         {
             this.repository = repo;
         }
 
-        public IActionResult Index()
-        {
-            return View(this.repository.Products);
-        }
+        //public IActionResult Index()
+        //{
+        //    return View(this.repository.Products);
+        //}
+
+        public ViewResult Index(int productPage = 1)
+        => View(repository.Products
+            .OrderBy(p => p.ProductID)
+            .Skip((productPage - 1) * PageSize)
+            .Take(PageSize));
 
         public IActionResult Privacy()
         {
