ec40b2de79a362f7006162dab4f26c8e50d7bc25
Subodh Shakya <subodhshakya@live.com>
2021-02-28 17:19:10 -0600

    Added view component for navigation menu

---
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Components
+{
+    public class NavigationMenuViewComponent
+    {
+        /// <summary>
+        /// This method is called when the component is used in a Razor view
+        /// and the result of the Invoke method is inserted into the HTML sent to the browser.
+        /// This component is for category list and for it to appear on all pages, use the view
+        /// component in shared layout, rather than specific view.
+        /// </summary>
+        /// <returns></returns>
+        public string Invoke()
+        {
+            return "Hello from the Nav View Component";
+        }
+    }
+}
