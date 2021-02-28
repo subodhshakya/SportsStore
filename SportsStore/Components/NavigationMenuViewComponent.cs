55a867b1cc7a904cb78961b508bc94e254440a55
Subodh Shakya <subodhshakya@live.com>
2021-02-28 17:47:49 -0600

    Added view page for the navigation menu component.

--- SportsStore/Components/NavigationMenuViewComponent.cs
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SportsStore.Components
 {
-    public class NavigationMenuViewComponent
+    public class NavigationMenuViewComponent : ViewComponent
     {
+        private IStoreRepository repository;
+
+        /// <summary>
+        /// Input parameter is IStoreRepository, when ASP.NET Core needs to
+        /// create an instance of view component class, it wll note the need to 
+        /// provide the value of this parameter and inspect the configuration in the 
+        /// Startup class to determine which implementation object should be used.
+        /// This is the dependency injection feature.
+        /// </summary>
+        /// <param name="repo"></param>
+        public NavigationMenuViewComponent(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+
         /// <summary>
         /// This method is called when the component is used in a Razor view
         /// and the result of the Invoke method is inserted into the HTML sent to the browser.
@@ -14,9 +31,12 @@ namespace SportsStore.Components
         /// component in shared layout, rather than specific view.
         /// </summary>
         /// <returns></returns>
-        public string Invoke()
+        public IViewComponentResult Invoke()
         {
-            return "Hello from the Nav View Component";
+            return View(repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
         }
     }
 }
